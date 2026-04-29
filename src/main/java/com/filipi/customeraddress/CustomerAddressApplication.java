ec70d8992aad352e6f894403b358df031743c3fc
Filipi Wanderley <135230785+FilipiWanderley@users.noreply.github.com>
2026-04-29 12:13:47 -0300

    feat: add Spring Boot application entry point

---
+++ src/main/java/com/filipi/customeraddress/CustomerAddressApplication.java
@@ -0,0 +1,12 @@
+package com.filipi.customeraddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAddressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerAddressApplication.class, args);
+    }
+}

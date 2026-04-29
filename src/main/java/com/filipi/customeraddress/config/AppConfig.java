b30705d1b354068ea14ca5a0f32ed962e99f0643
Filipi Wanderley <135230785+FilipiWanderley@users.noreply.github.com>
2026-04-29 12:49:32 -0300

    config: enable cache and configure API timeout

--- src/main/java/com/filipi/customeraddress/config/AppConfig.java
+++ src/main/java/com/filipi/customeraddress/config/AppConfig.java
@@ -3,16 +3,24 @@ package com.filipi.customeraddress.config;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
+@EnableCaching
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(10))
+                .build();
     }
 
     @Bean
